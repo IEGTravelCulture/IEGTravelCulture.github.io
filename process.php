b715b85870f10331b7d3c70b6b656c28251ae88a
FrankSquirel <frankwan27@gmail.com>
2015-03-30 01:41:30 -0400

    Testing php part 2

--- process.php
+++ process.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $name = $_POST['name'];
 $email = $_POST['email'];
 $telephone = $_POST['telephone'];
@@ -8,4 +8,14 @@ echo $name;
 echo $email;
 echo $telephone;
 echo $message;
+
+$email_message = "
+Name: ".$name."
+Email: ".$email."
+Telephone: ".$telephone."
+Message: ".$message."
+";
+
+mail("frankwan27@gmail.com", "New inquiry", $email_message);
+header("Location: /en/index.html");
 ?>
