471db066b2fe6efb7b5618d72c04325d14c3efdc
FrankSquirel <frankwan27@gmail.com>
2015-03-30 01:34:58 -0400

    Testing php

---
+++ process.php
@@ -0,0 +1,11 @@
+<?
+$name = $_POST['name'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$message = $_POST['message'];
+
+echo $name;
+echo $email;
+echo $telephone;
+echo $message;
+?>
